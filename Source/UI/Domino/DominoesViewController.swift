1f1059c7bda28e69883755bc3250286ec740f5e3
Ceri Hughes <github@cerihughes.co.uk>
2020-05-30 13:26:30 +0100

    Stack based scrolling view

--- Source/UI/Domino/DominoesViewController.swift
+++ Source/UI/Domino/DominoesViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class DominoesViewController: UIViewController {
     private lazy var dominoesView = DominoesView()
+    private var faceValue = DominoFaceView.Value.zero
 
     override func loadView() {
         view = dominoesView
@@ -17,15 +18,22 @@ class DominoesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        updateDominoes()
+
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
-        dominoesView.domino.addGestureRecognizer(gestureRecognizer)
+        dominoesView.addGestureRecognizer(gestureRecognizer)
     }
 
     @objc
     private func tapped() {
-        let numericValue = dominoesView.domino.face1.value.rawValue
-        let nextValue = DominoFaceView.Value(rawValue: numericValue + 1) ?? .zero
-        dominoesView.domino.face1.value = nextValue
-        dominoesView.domino.face2.value = nextValue
+        let numericValue = faceValue.rawValue
+        faceValue = DominoFaceView.Value(rawValue: numericValue + 1) ?? .zero
+        updateDominoes()
+    }
+
+    private func updateDominoes() {
+        let state = DominoView.State.faceUp(faceValue, faceValue)
+        dominoesView.remove(state: state)
+        dominoesView.add(state: state)
     }
 }
